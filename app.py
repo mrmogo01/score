4a57ab76a17a8b40f726777a69f93345abf1844c
mrmogo01 <155198794+mrmogo01@users.noreply.github.com>
2024-09-17 16:46:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="10">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <title>Leaderboard</title>
-<style>
-    body {
-        font-size: 55px;
-        font-family: 'Montserrat', sans-serif;
-        background-image: url('https://firebasestorage.googleapis.com/v0/b/digital-portfolio-f8705.appspot.com/o/background.jpg.png?alt=media&token=d8b6f379-bc70-402b-850b-a2cb9e95cd8d');
-        background-size: cover;
-        background-position: center;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-        text-align: center;
-    }
-    li::before {
-        content: "";
-    }
-    .leaderboard {
-        text-align: center;
-        width: 100%;
-    }
-    .search-form {
-        text-align: center; /* Центрирование формы */
-    }
-    .search-input {
-        width: 50%; /* Увеличение ширины поля ввода */
-        font-size: 30px; /* Увеличение размера шрифта в поле ввода */
-        padding: 10px; /* Добавление отступов внутри поля ввода */
-        margin-top: 20px; /* Отступ сверху */
-    }
-    .search-button {
-        font-size: 30px; /* Увеличение размера шрифта кнопки */
-        padding: 10px 20px; /* Увеличение отступов кнопки */
-    }
-</style>
+from flask import Flask, request, render_template
+import gspread
+from google.oauth2.service_account import Credentials
 
-</head>
-<body>
-<div class="leaderboard">
-    <h1>Общее количество баллов: {{ total_points }}</h1>
-    <div class="search-form">
-        <form action="/" method="get">
-            <input type="text" name="query" class="search-input" placeholder="Введите ФИО" value="{{ query }}">
-            <button type="submit" class="search-button">Поиск</button>
-        </form>
-    </div>
+app = Flask(__name__)
 
-    <ul>
-        {% for item in top_ten %}
-        <li>{{ loop.index }}) {{ item['ФИО'] }} - {{ item['сумма баллов'] }}</li>
-        {% endfor %}
-    </ul>
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = Credentials.from_service_account_file('credentials.json', scopes=scope)
+client = gspread.authorize(creds)
 
-    {% if filtered_data %}
-    <h2>Результаты поиска:</h2>
-    <ul>
-        {% for index, item in filtered_data %}
-        <li>{{ index }} {{ item['ФИО'] }} - {{ item['сумма баллов'] }}</li>
-        {% endfor %}
-    </ul>
-    {% endif %}
-</div>
-</body>
-</html>
+sheet_api = client.open('Баллы фест').sheet1
+
+@app.route('/', methods=['GET', 'POST'])
+def leaderboard():
+    query = request.args.get('query', '')
+    data = sheet_api.get_all_records()
+    sorted_data = sorted(data, key=lambda x: x['сумма баллов'], reverse=True)
+    total_points = sum(item['сумма баллов'] for item in sorted_data)
+
+    if query:
+        # Фильтрация данных для нахождения информации о человеке с соответствующим ФИО
+        filtered_data = [(index + 1, item) for index, item in enumerate(sorted_data) if query.lower() in item['ФИО'].lower()]
+        return render_template('leaderboard.html', total_points=total_points, top_ten=sorted_data[:10], filtered_data=filtered_data, query=query)
+    else:
+        return render_template('leaderboard.html', total_points=total_points, top_ten=sorted_data[:10], filtered_data=None, query='')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
